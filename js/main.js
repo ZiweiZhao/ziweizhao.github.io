4678edd40df2f269761c791f703083655cebb60d
ziwei.zhao <zhaoziwei2013@gmail.com>
2016-03-25 10:50:35 -0400

    profile photo

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
   }
 
 
-
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
